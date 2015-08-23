de46de7ac48c5183b20c706bbc91e3f4e2970e88
dmalko <dmalko@speroteck.com>
2015-08-24 00:47:15 +0300

    Галерея

--- sources/js/content-height.js
+++ sources/js/content-height.js
@@ -1,17 +1,17 @@
 /**
  * Created by dmalko on 22.08.2015.
  */
-jQuery(window).load(function(){
+$(function() {
     console.log('content page height initialization');
 
-    var addComment = jQuery('.comments-list');
-    var scrollContent = jQuery('.scroll-wrapper');
-    var windowHeight = jQuery(window).height();
+    var addComment = $('.comments-list');
+    var scrollContent = $('.scroll-wrapper');
+    var windowHeight = $(window).height();
     var staticElements = ['header', 'footer', '.pager'];
     var indent = 0;
 
-    jQuery.each(staticElements, function(index, value){
-        indent = indent + jQuery(value).height();
+    $.each(staticElements, function(index, value){
+        indent = indent + $(value).height();
     });
 
     scrollContent.height(windowHeight - indent).jScrollPane({
---
+++ sources/js/gallery.js
@@ -0,0 +1,6 @@
+/**
+ * Created by dmalko on 24.08.2015.
+ */
+$(function() {
+    $('a.gallery').colorbox({rel:'gal'});
+});
