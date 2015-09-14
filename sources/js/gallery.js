03041b2f394af98c8b6bf132a60f0745e0b4e658
dmalko <malko.denis1987@gmail.com>
2015-09-14 22:41:41 +0300

    фиксы 14.09

--- sources/js/gallery.js
+++ sources/js/gallery.js
@@ -2,5 +2,8 @@
  * Created by dmalko on 24.08.2015.
  */
 $(function() {
-    $('a.gallery').colorbox();
+    $('a.gallery').colorbox({
+        maxWidth: '80%',
+        maxHeight: '80%'
+    });
 });
